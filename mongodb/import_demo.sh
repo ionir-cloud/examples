a1df7ca7edafa975b21d51fc275b816aadbcb24a
rdxbarakn <77359817+rdxbarakn@users.noreply.github.com>
2021-12-15 17:31:26 +0200

    Update import_demo.sh

--- mongodb/import_demo.sh
+++ mongodb/import_demo.sh
@@ -4,7 +4,7 @@ POD_NAME="mongodb-0"
 
 echo "getting the data"
 kubectl -n ${POD_NAMESPACE} exec ${POD_NAME} -- bash -c "cd /tmp \
-&& apt-get update && apt-get install wget \
+&& apt-get update && apt-get install wget && apt-get install gzip \
 && wget https://datasets.imdbws.com/name.basics.tsv.gz \
 $$ wget https://datasets.imdbws.com/title.akas.tsv.gz \
 $$ wget https://datasets.imdbws.com/title.basics.tsv.gz \
@@ -15,13 +15,13 @@ $$ wget https://datasets.imdbws.com/title.ratings.tsv.gz"
 
 echo "extracting the data"
 kubectl -n ${POD_NAMESPACE} exec ${POD_NAME} -- bash -c "cd /tmp \
-&& tar -xvf name.basics.tsv.gz \
-$$ tar -xvf title.akas.tsv.gz \
-$$ tar -xvf title.basics.tsv.gz \
-$$ tar -xvf title.crew.tsv.gz \
-$$ tar -xvf title.episode.tsv.gz \
-$$ tar -xvf title.principals.tsv.gz \
-$$ tar -xvf title.ratings.tsv.gz"
+&& gzip -dk name.basics.tsv.gz \
+$$ gzip -dk title.akas.tsv.gz \
+$$ gzip -dk title.basics.tsv.gz \
+$$ gzip -dk title.crew.tsv.gz \
+$$ gzip -dk title.episode.tsv.gz \
+$$ gzip -dk title.principals.tsv.gz \
+$$ gzip -dk title.ratings.tsv.gz"
 
 echo "importing the data"
 kubectl -n ${POD_NAMESPACE} exec ${POD_NAME} -- bash -c "
